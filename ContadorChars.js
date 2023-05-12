7963e13d1a40f8c055a92c72023335da6c113296
Inaue <cafe.ina@outlook.com>
2023-05-12 00:20:16 +0000

    Update css + html and JS reusable.

--- ContadorChars.js
+++ ContadorChars.js
@@ -1,17 +1,16 @@
-function contaChars()
+function contaChars(area_txt, num_lim, rad_sim, count)
 {
-    var tamString   = document.getElementById("caixa_de_texto").value.length;
-    var valorCaixa  = document.getElementById("caixa_limite").value;
-    var valorSim    = document.getElementById("radio_sim").checked;
-    var contador    = document.getElementById("chars_digitados");
-    var txt_aviso   = document.getElementById("txt_aviso");
-    var limite      = (valorSim) ? valorCaixa : '';
+    var tamString   = document.getElementById(area_txt).value.length;
+    var valor_lim   = document.getElementById(num_lim).value;
+    var valorSim    = document.getElementById(rad_sim).checked;
+    var contador    = document.getElementById(count);
+    var limite      = (valorSim) ? valor_lim : '';
     var c_digitar   = (limite == '') ? 0 : limite - tamString;
     var textoAdd;
     var cor;
     
-    cor =   (limite == '') ?    'white' :
-            (c_digitar > 0) ?   'white' : 'red';
+    cor =   (limite == '') ?    'gray' :
+            (c_digitar > 0) ?   'gray' : 'red';
 
     textoAdd    =   (limite == '') ?
                         (tamString == 0) ?  "Não há caracteres digitados." :
@@ -23,33 +22,27 @@ function contaChars()
                     (c_digitar > 0) ?   "Você ainda pode digitar " + c_digitar + " caracteres."
                     : "Você passou " + (-c_digitar) + " caracteres do limite permitido.";
 
-    txt_aviso.style.display =   (valorSim) ?
-                                    (limite == '') ?    'block' : 'none'
-                                : txt_aviso.style.display;
-
     contador.innerHTML      = textoAdd;
     contador.style.color    = cor;
 }
 
-function temLimite()
+function temLimite(area_txt, num_lim, rad_sim, count)
 {
-    var valorSim    = document.getElementById("radio_sim").checked;
-    var valorNao    = document.getElementById("radio_nao").checked;
-    var caixa       = document.getElementById("caixa_limite");
-    var txt_aviso   = document.getElementById("txt_aviso");
+    var valorSim    = document.getElementById(rad_sim).checked;
+    var caixa_lim   = document.getElementById(num_lim);
 
-    if (valorSim)
-    {
-        caixa.style.display = "inline";
-        
-        caixa.value == "" ?
-            txt_aviso.style.display = "block" :
-            contaChars();
-    }
-    else if (valorNao)
-    {
-        caixa.style.display     = "none";
-        txt_aviso.style.display = "none";
-        contaChars();
-    }
+    caixa_lim.style.display = valorSim ? 'inline' : 'none';
+    contaChars(area_txt, num_lim, rad_sim, count);
+}
+
+let display_txt     = function(alert, num_lim, rad_sim)
+{
+    let valor_lim   = document.getElementById(num_lim).value;
+    let valorSim    = document.getElementById(rad_sim).checked;
+    let txt_aviso   = document.getElementById(alert);
+    let limite      = (valorSim) ? valor_lim : '';
+
+    txt_aviso.style.display =   (valorSim) ?
+                                    (limite == '') ?    'block' : 'none'
+                                : txt_aviso.style.display;
 }
